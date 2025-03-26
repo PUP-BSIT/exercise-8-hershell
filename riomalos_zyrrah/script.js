37b8316eb3d6b32f89029a50d8ef8cc444e34505
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2025-03-26 18:43:31 +0800

    add function to append comment (#6)

--- riomalos_zyrrah/script.js
+++ riomalos_zyrrah/script.js
@@ -13,3 +13,25 @@ function checkForm() {
     button.classList.add("disabled");
   }
 }
+
+let commentForm = document.querySelector(".comment-form form");
+let commentsSection = document.querySelector(".teammate-comment");
+
+function appendComment(event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name_comment").value;
+  let comment = document.getElementById("comment").value;
+
+  let newComment = document.createElement("p");
+  newComment.textContent = `${comment} -${name}`;
+
+  commentsSection.appendChild(newComment);
+
+  document.getElementById("name_comment").value = "";
+  document.getElementById("comment").value = "";
+
+  checkForm();
+}
+
+commentForm.addEventListener("submit", appendComment);
