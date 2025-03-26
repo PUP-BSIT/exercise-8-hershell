bf1607054f5d287ece4fc9075929edf7e5c39456
florido-maydelyn <maydelynflorido07@gmail.com>
2025-03-26 12:46:17 +0800

    update js file (#3)
    * add addElement function in js file
    
    * add clearInput function in js file
    
    * add changes in js file
    
    * update js file

--- florido_maydelyn/script.js
+++ florido_maydelyn/script.js
@@ -10,5 +10,27 @@ function toggleButton() {
   }
 }
 
-document.getElementById("comment_name").addEventListener("input", toggleButton);
-document.getElementById("comment").addEventListener("input", toggleButton);
+function addElement() {
+  let nameInput = document.getElementById("comment_name").value;
+  let commentInput = document.getElementById("comment").value;
+
+  let commentSection = document.querySelector(".groupmate-comments");
+  let commentHolder = document.createElement("p");
+  commentSection.append(commentHolder);
+  commentHolder.textContent = nameInput + ": " + commentInput;
+
+  clearInput();
+  toggleButton();
+}
+
+function clearInput() {
+  document.getElementById("comment_name").value = "";
+  document.getElementById("comment").value = "";
+}
+
+document
+  .getElementById("comment_name").addEventListener("input", toggleButton);
+document
+  .getElementById("comment").addEventListener("input", toggleButton);
+document
+  .getElementById("comment_button").addEventListener("click", addElement);
