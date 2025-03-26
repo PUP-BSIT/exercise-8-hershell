3052b9c084bcef1daabc6984d3628274afa1e643
durante-stephanie <durantestephanie07@gmail.com>
2025-03-26 16:09:00 +0800

    update js file (#10)
    * add addComment function in js file
    
    * add changes in js file

--- durante_stephanie/index.js
+++ durante_stephanie/index.js
@@ -1,8 +1,9 @@
-function validateComment() {
-  let commentName = document.getElementById("comment_name");
-  let commentMessage = document.getElementById("comment_form");
-  let commentButton = document.getElementById("comment_button");
+const commentName = document.getElementById("comment_name");
+const commentMessage = document.getElementById("comment_form");
+const commentButton = document.getElementById("comment_button");
+const commentContainer = document.getElementById("comment_list");
 
+function validateComment() {
   if (commentName.value.length && commentMessage.value.length) {
     commentButton.disabled = false;
   } else {
@@ -10,9 +11,22 @@ function validateComment() {
   }
 }
 
-document
-  .getElementById("comment_name")
-  .addEventListener("input", validateComment);
-document
-  .getElementById("comment_form")
-  .addEventListener("input", validateComment);
+function addComment() {
+  const newComment = {
+    name: commentName.value,
+    comment: commentMessage.value,
+  };
+
+  const newCommentElement = document.createElement("li");
+  newCommentElement.innerHTML = `${newComment.name}: ${newComment.comment}`;
+
+  commentContainer.appendChild(newCommentElement);
+
+  commentName.value = "";
+  commentMessage.value = "";
+  validateComment();
+}
+
+commentName.addEventListener("input", validateComment);
+commentMessage.addEventListener("input", validateComment);
+commentButton.addEventListener("click", addComment);
